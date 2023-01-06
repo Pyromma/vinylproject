00a86f05ff18b61f1574303b21b07b229ca8fee5
Pyromma <struzik.wroc@gmail.com>
2023-01-06 16:29:59 +0100

    inj

--- navbar.php
+++ navbar.php
@@ -1,7 +1,7 @@
 <div class="mainbar" id="navbar">
-    <a id="navburger" href="javascript:void(0);" onclick="navBar()"><img id="minilogo" src="/vinylproject/images/page/navburger.png"></a>
-    <a href="/vinylproject/home.php"><img id="minilogo" src="/vinylproject/images/page/logo3.png"></a>
-    <a class="navButton" href="/vinylproject/home.php">Home</a>
+    <a id="navburger" href="javascript:void(0);" onclick="navBar()"><img id="minilogo" src="images/page/navburger.png"></a>
+    <a href="home.php"><img id="minilogo" src="images/page/logo3.png"></a>
+    <a class="navButton" href="home.php">Home</a>
     <a class="navButton" href="">Library</a>
     <?php
         if($_SESSION['administrator'] == true)
@@ -11,8 +11,8 @@
         <div class="dropdown-content">
             <a class="navButton" href="#">Profile</a>
             <a class="navButton" href="#">Rentals</a>
-            <a class="navButton" href="/vinylproject/authenticate.php?logout=true">Logout</a>
+            <a class="navButton" href="authenticate.php?logout=true">Logout</a>
         </div>
     </div>
 </div>
-<script src="/vinylproject/js.js"></script>
+<script src="js.js"></script>
